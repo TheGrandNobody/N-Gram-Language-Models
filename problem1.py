4a29691713b3c74fdf2b52daa05c429f6a5c1038
TheGrandNobody <thegrandnobody@protonmail.com>
2023-04-18 18:31:57 +0200

    Begin problem5.py

--- problem1.py
+++ problem1.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-import json
-
 """
 NLP A2: N-Gram Language Models
 
@@ -9,16 +7,18 @@ NLP A2: N-Gram Language Models
 DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 """
 
-word_index_dict = {}
+import json
 
-# Read the text file and create a dictionary with the words as keys and the indices as values
-with open("brown_vocab_100.txt", "r") as f:
-    for i, line in enumerate(f):
-        word_index_dict[line.rstrip()] = i
-# Write the dictionary to a text file
-with open("word_to_index_100.txt", "w") as f:
-    json.dump(word_index_dict, f, indent=4)
+if __name__ == "__main__":
+    word_index_dict = {}
+    # Read the text file and create a dictionary with the words as keys and the indices as values
+    with open("brown_vocab_100.txt", "r") as f:
+        for i, line in enumerate(f):
+            word_index_dict[line.rstrip()] = i
+    # Write the dictionary to a text file
+    with open("word_to_index_100.txt", "w") as f:
+        json.dump(word_index_dict, f, indent=4)
 
-print(word_index_dict['all'])
-print(word_index_dict['resolution'])
-print(len(word_index_dict))
+    print(word_index_dict['all'])
+    print(word_index_dict['resolution'])
+    print(len(word_index_dict))
--- problem2.py
+++ problem2.py
@@ -12,35 +12,36 @@ import json
 import numpy as np
 from generate import GENERATE
 
-# load the indices dictionary
-word_index_dict = {}
-with open("brown_vocab_100.txt", "r") as f:
-    for i, line in enumerate(f):
-        word_index_dict[line.rstrip()] = i
+if __name__ == "__main__":
+    # load the indices dictionary
+    word_index_dict = {}
+    with open("brown_vocab_100.txt", "r") as f:
+        for i, line in enumerate(f):
+            word_index_dict[line.rstrip()] = i
 
-# load all sentences
-with open("brown_100.txt", "r") as f:
-    sentences = f.read().splitlines()
+    # load all sentences
+    with open("brown_100.txt", "r") as f:
+        sentences = f.read().splitlines()
 
-# initialize counts to a zero vector
-counts = np.zeros(len(word_index_dict.keys())) 
+    # initialize counts to a zero vector
+    counts = np.zeros(len(word_index_dict.keys())) 
 
-# iterate through file and update counts
-for sen in sentences:
-    words = sen.lower().strip().split(" ")
-    for word in words:
-        counts[word_index_dict[word]] += 1
+    # iterate through file and update counts
+    for sen in sentences:
+        words = sen.lower().strip().split(" ")
+        for word in words:
+            counts[word_index_dict[word]] += 1
 
-# normalize and writeout counts. 
-probs = counts / np.sum(counts)
+    # normalize and writeout counts. 
+    probs = counts / np.sum(counts)
 
-# write probs as a dict into a text file
-word_probs_dict = {}
-for word in word_index_dict:
-    word_probs_dict[word] = probs[word_index_dict[word]]
-with open("unigram_probs.txt", "w") as f:
-    json.dump(word_probs_dict, f, indent=4)
+    # write probs as a dict into a text file
+    word_probs_dict = {}
+    for word in word_index_dict:
+        word_probs_dict[word] = probs[word_index_dict[word]]
+    with open("unigram_probs.txt", "w") as f:
+        json.dump(word_probs_dict, f, indent=4)
 
-# verification
-print("%.8f" % word_probs_dict["all"])
-print("%.8f" % word_probs_dict["resolution"])
+    # verification
+    print("%.8f" % word_probs_dict["all"])
+    print("%.8f" % word_probs_dict["resolution"])
