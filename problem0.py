df3f84dd8ea2722a092f66c71d505e57ced63680
TheGrandNobody <thegrandnobody@protonmail.com>
2023-04-18 16:09:56 +0200

    Improve

--- problem0.py
+++ problem0.py
@@ -36,12 +36,11 @@ def main():
     print("10 most frequent parts of speech: ", most_frequent)
 
     # Plot the frequency distribution of the corpus, news genre and romance on both linear and log-log scale
-    title = ["Linear F. Distribution: ", "Log-Log F. Distribution: "]
     genre = ["Corpus", "News", "Romance"]
     fig, axs = plt.subplots(2, 3)
     for i in range(2):
         for j in range(3):
-            axs[i,j].set_title(title[i] + genre[j])
+            axs[i,j].set_title(genre[j])
             if i == 0:
                 if j == 0:
                     axs[i,j].plot(range(1, types + 1), sorted(fdist.values(), reverse=True))
@@ -56,7 +55,7 @@ def main():
                     axs[i,j].loglog(range(1, len(fdist_news) + 1), sorted(fdist_news.values(), reverse=True))
                 else:
                     axs[i,j].loglog(range(1, len(fdist_romance) + 1), sorted(fdist_romance.values(), reverse=True))
-            axs[i,j].set_xlabel("Position in the frequency distribution")
+            axs[i,j].set_xlabel("Position")
             axs[i,j].set_ylabel("Frequency")
     fig.tight_layout()
     fig.savefig("freq_dist.png")
